03526b3e5de19199aff5c90110674211aebde051
Ramón <ramonarj@ucm.es>
2020-04-19 13:39:30 +0200

    Event y TrackerManager

---
+++ Rat Harvest/Assets/Scripts/Tracker/Event.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Clase para representar un evento
+ */
+public class Event
+{
+
+}
---
+++ Rat Harvest/Assets/Scripts/Tracker/TrackerManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrackerManager : MonoBehaviour
+{
+    private static TrackerManager instance = null;
+    //Constructora privada
+    private TrackerManager()
+    {
+    }
+
+    //Para obtener la instancia del Singleton
+    public static TrackerManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new TrackerManager();
+            }
+            return instance;
+        }
+    }
+
+
+    //Para obtener la instancia del Singleton
+    public static void TrackEvent (Event e)
+    {
+        //Añadirlo a la cola o algo sabe
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
