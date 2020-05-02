a9c99674c8429da0409523aa0bade20110875b6b
rebo95 <arebored@ucm.es>
2020-05-02 17:34:45 +0200

    Tracker events added

--- Rat Harvest/Assets/Scripts/Tracker/TrackerEvent.cs
+++ Rat Harvest/Assets/Scripts/Tracker/TrackerEvent.cs
@@ -99,3 +99,293 @@ public class EndGameEvent : TrackerEvent
         return "";
     }
 }
+
+
+/// <summary>
+/// Clase que define el evento que determina cuando se interactua con el botón
+/// de play y lanza la pantalla tutorial de What to do.
+/// </summary>
+public class WhatToDoShownEvent : TrackerEvent
+{
+    /// <summary>
+    /// Traduce el evento al formato Json.
+    /// </summary>
+    /// <returns>Una cadena de texto en formato Json.</returns>
+    public override string ToCSV()
+    {
+        // TODO
+        return "";
+    }
+
+    /// <summary>
+    /// Traduce el evento al formato CSV.
+    /// </summary>
+    /// <returns>Una cadena de texto en formato CSV.</returns>
+    public override string ToJson()
+    {
+        // TODO
+        return "";
+    }
+}
+
+
+/// <summary>
+/// Clase que define el evento que determina cuando se cumple el primer minuto de partida.
+/// </summary>
+public class FirstMinEvent : TrackerEvent
+{
+    /// <summary>
+    /// Traduce el evento al formato Json.
+    /// </summary>
+    /// <returns>Una cadena de texto en formato Json.</returns>
+    public override string ToCSV()
+    {
+        // TODO
+        return "";
+    }
+
+    /// <summary>
+    /// Traduce el evento al formato CSV.
+    /// </summary>
+    /// <returns>Una cadena de texto en formato CSV.</returns>
+    public override string ToJson()
+    {
+        // TODO
+        return "";
+    }
+}
+
+
+/// <summary>
+/// Clase que define el evento que determina cuando cuando se interactúa con 
+/// un parterre.
+/// </summary>
+public class UserPlantEvent : TrackerEvent
+{
+    /// <summary>
+    /// Traduce el evento al formato Json.
+    /// </summary>
+    /// <returns>Una cadena de texto en formato Json.</returns>
+    public override string ToCSV()
+    {
+        // TODO
+        return "";
+    }
+
+    /// <summary>
+    /// Traduce el evento al formato CSV.
+    /// </summary>
+    /// <returns>Una cadena de texto en formato CSV.</returns>
+    public override string ToJson()
+    {
+        // TODO
+        return "";
+    }
+}
+
+public class UserHarvestEvent : TrackerEvent
+{
+    /// <summary>
+    /// Traduce el evento al formato Json.
+    /// </summary>
+    /// <returns>Una cadena de texto en formato Json.</returns>
+    public override string ToCSV()
+    {
+        // TODO
+        return "";
+    }
+
+    /// <summary>
+    /// Traduce el evento al formato CSV.
+    /// </summary>
+    /// <returns>Una cadena de texto en formato CSV.</returns>
+    public override string ToJson()
+    {
+        // TODO
+        return "";
+    }
+}
+
+public class UserStoreEvent : TrackerEvent
+{
+    /// <summary>
+    /// Traduce el evento al formato Json.
+    /// </summary>
+    /// <returns>Una cadena de texto en formato Json.</returns>
+    public override string ToCSV()
+    {
+        // TODO
+        return "";
+    }
+
+    /// <summary>
+    /// Traduce el evento al formato CSV.
+    /// </summary>
+    /// <returns>Una cadena de texto en formato CSV.</returns>
+    public override string ToJson()
+    {
+        // TODO
+        return "";
+    }
+}
+
+public class WeaponShotEvent : TrackerEvent
+{
+    /// <summary>
+    /// Traduce el evento al formato Json.
+    /// </summary>
+    /// <returns>Una cadena de texto en formato Json.</returns>
+    public override string ToCSV()
+    {
+        // TODO
+        return "";
+    }
+
+    /// <summary>
+    /// Traduce el evento al formato CSV.
+    /// </summary>
+    /// <returns>Una cadena de texto en formato CSV.</returns>
+    public override string ToJson()
+    {
+        // TODO
+        return "";
+    }
+}
+
+public class ReadTime : TrackerEvent
+{
+    /// <summary>
+    /// Traduce el evento al formato Json.
+    /// </summary>
+    /// <returns>Una cadena de texto en formato Json.</returns>
+    public override string ToCSV()
+    {
+        // TODO
+        return "";
+    }
+
+    /// <summary>
+    /// Traduce el evento al formato CSV.
+    /// </summary>
+    /// <returns>Una cadena de texto en formato CSV.</returns>
+    public override string ToJson()
+    {
+        // TODO
+        return "";
+    }
+}
+
+public class MaxLoadTimeEvent : TrackerEvent
+{
+    /// <summary>
+    /// Traduce el evento al formato Json.
+    /// </summary>
+    /// <returns>Una cadena de texto en formato Json.</returns>
+    public override string ToCSV()
+    {
+        // TODO
+        return "";
+    }
+
+    /// <summary>
+    /// Traduce el evento al formato CSV.
+    /// </summary>
+    /// <returns>Una cadena de texto en formato CSV.</returns>
+    public override string ToJson()
+    {
+        // TODO
+        return "";
+    }
+}
+
+public class PlantedFirstMinEvent : TrackerEvent
+{
+    /// <summary>
+    /// Traduce el evento al formato Json.
+    /// </summary>
+    /// <returns>Una cadena de texto en formato Json.</returns>
+    public override string ToCSV()
+    {
+        // TODO
+        return "";
+    }
+
+    /// <summary>
+    /// Traduce el evento al formato CSV.
+    /// </summary>
+    /// <returns>Una cadena de texto en formato CSV.</returns>
+    public override string ToJson()
+    {
+        // TODO
+        return "";
+    }
+}
+
+public class ShotFirstMinEvent : TrackerEvent
+{
+    /// <summary>
+    /// Traduce el evento al formato Json.
+    /// </summary>
+    /// <returns>Una cadena de texto en formato Json.</returns>
+    public override string ToCSV()
+    {
+        // TODO
+        return "";
+    }
+
+    /// <summary>
+    /// Traduce el evento al formato CSV.
+    /// </summary>
+    /// <returns>Una cadena de texto en formato CSV.</returns>
+    public override string ToJson()
+    {
+        // TODO
+        return "";
+    }
+}
+
+public class PlayerPosFirstMinEvent : TrackerEvent
+{
+    /// <summary>
+    /// Traduce el evento al formato Json.
+    /// </summary>
+    /// <returns>Una cadena de texto en formato Json.</returns>
+    public override string ToCSV()
+    {
+        // TODO
+        return "";
+    }
+
+    /// <summary>
+    /// Traduce el evento al formato CSV.
+    /// </summary>
+    /// <returns>Una cadena de texto en formato CSV.</returns>
+    public override string ToJson()
+    {
+        // TODO
+        return "";
+    }
+}
+
+public class PointsMinEvent : TrackerEvent
+{
+    /// <summary>
+    /// Traduce el evento al formato Json.
+    /// </summary>
+    /// <returns>Una cadena de texto en formato Json.</returns>
+    public override string ToCSV()
+    {
+        // TODO
+        return "";
+    }
+
+    /// <summary>
+    /// Traduce el evento al formato CSV.
+    /// </summary>
+    /// <returns>Una cadena de texto en formato CSV.</returns>
+    public override string ToJson()
+    {
+        // TODO
+        return "";
+    }
+}
